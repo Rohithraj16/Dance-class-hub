238cb1b1f21af4bb7529e951c2a20e200947bc19
Rohithraj16 <168125585+Rohithraj16@users.noreply.github.com>
2025-03-06 21:40:32 +0530

    HeroSection : completed with title and small paragraph

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -10,12 +10,14 @@ function HeroSection() {
 			/>
 			<div className="py-4 relative z-10 w-full text-center">
 				<h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
-					Master the art of music
+					The Science and Soul of Music
 				</h1>
 				<p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
-					Dive into our comprehensive music courses and transform your musical
-					journey today. Whether you're a beginner or looking to refine your
-					skills, join us to unlock your true potential.
+					Explore our diverse range of music courses designed to elevate your
+					skills and passion. No matter where you are in your musical journey,
+					our expert guidance will help you grow, innovate, and reach new
+					heights in your artistry. Start today and unleash your full creative
+					potential!
 				</p>
 				<div className="mt-4">
 					<Link href={"/courses"}>
